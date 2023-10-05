086e7ea2a6048b0cc7278a5732f12661544c26d5
HernaldoMeneses <hernaldomeneses@gmail.com>
2023-10-05 06:31:18 -0300

    ./HernaldoMeneses

--- ABC-I/R/01_init_10.r
+++ ABC-I/R/01_init_10.r
@@ -14,19 +14,41 @@
 ########### - Armazenamento.
 ###
 ## -- DataFrames
-path <- "C:/"
-file <- "filename.csv"
+path <- "C:\\Users\\herna\\OneDrive\\Github\\Learning\\ABC-I\\R\\csv"
+file <- "election_results_presidential.csv"
 
 setwd(path)
 df <- read.csv(file)
 
-view(df)
+?View
+
+View(df)
 str(df)
 summary(df)
 
 df
-df[1]
-df$nomeColuna
+df[14]
+#o $ auxilia na seleção da coluna ao digitar
+df$candidate_name
+str(df$candidate_name)
+summary(df$candidate_name)
+
+class(df$candidate_name)
+class(df[14])
+
+fatia_df <- df
+fatia_df <- df[1:3]
+fatia_df
+fatia_df[2] <- NULL
+fatia_df
+fatia_df$newColumns <- "a"
+fatia_df
+class(fatia_df$newColumns)
+fatia_df$newColumns <- NULL
+fatia_df$newColumns <- NA
+fatia_df$newColumns[1:3] <- c(2,4,5)
+fatia_df
+class(fatia_df$newColumns)
 
 Col1 <- df[1]
 Col2 <- df$nomeColuna
