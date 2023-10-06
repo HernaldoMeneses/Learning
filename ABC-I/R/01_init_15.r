c5511795da41eb65ed69197d3e22afe0e9db3734
HernaldoMeneses <hernaldomeneses@gmail.com>
2023-10-06 07:01:04 -0300

    Update 01_init_15.r

--- ABC-I/R/01_init_15.r
+++ ABC-I/R/01_init_15.r
@@ -15,11 +15,33 @@
 ###
 ## -- if, for, while
 
-#se algo é verdade faça 
-if (5==5) "sim"
-if (5==5) "não"
+# if TRUE do
+if (5==5) "iqual"
+if (5!=5) "not iqual"
+if (5!=5) "empyt"
+if (5==5) 1+1
+# GoodCood
+if (5==5) {
+    "igual"
+    }
+# Try
+if (5==5) {
+    "igual"
+    if (6==6) {
+        "igual too"
+        }
+    
+    }
+# Supouse
+ages <- c(25,30)
+names <- c("Jo","Nho")
+df <- data.frame(names,ages)
 
-
+if (df$ages[df$names=="Nho"] > df$ages[df$names=="Jo"]) {
+    "Nho ++**++     &&&     Jo --**--"    
+} else {
+    "Nho --**--     &&&     Jo ++**++"    
+}
 
 ##
 ###
