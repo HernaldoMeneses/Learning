ea66dc1b3d57c23feb1f71755d59f3b2c003fd33
HernaldoMeneses <hernaldomeneses@gmail.com>
2023-10-11 05:30:35 -0300

    ./HMeneses

---
+++ ABC-I/Shell/install.sh
@@ -0,0 +1,12 @@
+
+#sudo apt-get install nome_shell
+
+#sudo apt-get install sh
+sudo apt-get install bash
+#sudo apt-get install ksh
+#sudo apt-get install zsh
+#sudo apt-get install csh
+#sudo apt-get install tcsh
+
+#Try
+csh
