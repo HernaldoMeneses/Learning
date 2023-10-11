0a26b9470f2b02ad0e4f1b1ae0c0a5499c0f6ad5
HernaldoMeneses <hernaldomeneses@gmail.com>
2023-10-11 07:01:19 -0300

    ./Meneses_init_Java

---
+++ ABC-I/Java/init_abc_java/src/main/java/com/mycompany/init_abc_java/Init_abc_java.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.init_abc_java;
+
+/**
+ *
+ * @author herna
+ */
+public class Init_abc_java {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
