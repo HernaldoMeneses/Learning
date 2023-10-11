e7b242c7836314c88709b0b969293127546bf480
HernaldoMeneses <hernaldomeneses@gmail.com>
2023-10-11 06:48:51 -0300

    ./Meneses

---
+++ ABC-I/C#/01_init_01.cs
@@ -0,0 +1,37 @@
+//########### - Guia Inicial.
+//#
+//# - Objetivo: Primeiro contato com a linguagem R e O RStudio
+//# - Obs     : Sem Exaurimento.
+//# -     * Topicos Abordados
+//# -         Variáveis
+//# -             Criação
+//# -             Atribuição
+//# -             R Case Sensitive
+//# -             Strings
+//#
+//# - Infrastrutura de compilação e informações ao final do Script.
+//########### --------------------------------------------------------------------Thanks.
+
+//########### - Parte I - inclusao das bibliotecas.
+using System;
+
+//Class Main
+class main{
+    static void Main(){
+        Console.Write("Hello..");
+    }
+}
+
+
+//########### --------------------------------------------------------------------Reloaded.
+
+
+//########### - Compile_By.
+//#
+//# - SO:  Windows 10 Pro.22H2
+//# - IDE: RStudio.2023.09.0
+//# - Obs: no bugs.
+//# - Obs: 01/10/2023.
+//#
+//# - Hello... please if you find something wrong,  contact-me.
+//########### --------------------------------------------------------------------Follow the white rabbit.
