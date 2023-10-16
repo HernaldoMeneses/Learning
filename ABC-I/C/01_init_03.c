2cc878f42eda3156857117960b31e85e358f6a73
HernaldoMeneses <hernaldomeneses@gmail.com>
2023-10-16 06:54:46 -0300

    ./H.Meneses

---
+++ ABC-I/C/01_init_03.c
@@ -0,0 +1,42 @@
+//########### - Guia Inicial.
+//#
+//# - Objetivo: Primeiro contato com a linguagem R e O RStudio
+//# - Obs     : Sem Exaurimento.
+//# -     * Topicos Abordados
+//# -         Variáveis
+//# -             Criação
+//# -             Atribuição
+//# -             R Case Sensitive
+//# -             Strings
+//#
+//# - Infrastrutura de compilação e informações ao final do Script.
+//########### --------------------------------------------------------------------Thanks.
+
+//########### - Parte I - inclusao das bibliotecas.
+#include <stdio.h>
+
+//#main function
+/*
+Longs Coments
+*/
+int main() {
+	printf("Hello.\n");
+    printf("Hello.");
+    printf("Hello.\n");
+
+    return 0;
+}
+
+
+//########### --------------------------------------------------------------------Reloaded.
+
+
+//########### - Compile_By.
+//#
+//# - SO:  Windows 10 Pro.22H2
+//# - IDE: RStudio.2023.09.0
+//# - Obs: no bugs.
+//# - Obs: 01/10/2023.
+//#
+//# - Hello... please if you find something wrong,  contact-me.
+//########### --------------------------------------------------------------------Follow the white rabbit.
