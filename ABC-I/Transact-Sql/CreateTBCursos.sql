8fd6666a6406f5ee0929db95a4a1e3024035fe58
HernaldoMeneses <hernaldomeneses@gmail.com>
2023-10-17 06:47:38 -0300

    ./HMeneses

---
+++ ABC-I/Transact-Sql/CreateTBCursos.sql
@@ -0,0 +1,28 @@
+USE [SQL_DB_1]
+GO
+
+/****** Object:  Table [dbo].[Cursos]    Script Date: 17/10/2023 06:45:46 ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Cursos]') AND type in (N'U'))
+DROP TABLE [dbo].[Cursos]
+GO
+
+/****** Object:  Table [dbo].[Cursos]    Script Date: 17/10/2023 06:45:46 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Cursos](
+	[id_curso] [int] NOT NULL,
+	[nome_curso] [varchar](200) NOT NULL,
+	[data_cadastro] [datetime] NOT NULL,
+	[login_cadastro] [varchar](15) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id_curso] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
