752763232b215303496fbfdcf5d9a1f39234caff
HernaldoMeneses <hernaldomeneses@gmail.com>
2023-10-24 06:24:24 -0300

    Create 01_init_01.jl

---
+++ ABC-I/Julia/01_init_01.jl
@@ -0,0 +1,19 @@
+pwd()
+my_str0 = "Hello"
+my_str0 = " "
+my_str2 = "World"
+my_str3 = "!"
+println(my_str0)
+print(my_str0)
+#To run. Open prompt (win + r)
+#Go to corect hard disc (E: + enter)
+#Go to directory of this file (cd E:\Github\Learning\ABC-I\Julia)
+#Open Julia (julia + enter)
+# type 
+#Julia en include("01_init_01.jl")
+#enter
+#Linear__
+
+#You can use Julia to navegate directorys too
+pwd() 
+cd()
