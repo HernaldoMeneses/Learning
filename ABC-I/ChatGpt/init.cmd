9b7b0c22dc4a69aea277eb2897762ff2f5cf1486
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2023-10-31 18:10:34 -0300

    Create init.cmd

---
+++ ABC-I/ChatGpt/init.cmd
@@ -0,0 +1,10 @@
+
+https://platform.openai.com/docs/api-reference/authentication
+
+pip install --upgrade pip
+pip install
+echo "Enter to continue..."
+read
+npm install openai@^4.0.0
+echo "Enter to continue..."
+read
