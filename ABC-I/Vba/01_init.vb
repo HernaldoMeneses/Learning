7596a99deaa63f0d2adb656e4b792c0e79ba004c
HernaldoMeneses <hernaldomeneses@gmail.com>
2023-11-01 07:15:06 -0300

    ./HMeneses

---
+++ ABC-I/Vba/01_init.vb
@@ -0,0 +1,19 @@
+Sub Macro1()
+'
+' Macro1 Macro
+' Edita green
+'
+' Atalho do teclado: Ctrl+Shift+L
+'
+    With Selection.Interior
+        .Pattern = xlSolid
+        .PatternColorIndex = xlAutomatic
+        .ThemeColor = xlThemeColorAccent3
+        .TintAndShade = -0.499984740745262
+        .PatternTintAndShade = 0
+    End With
+    With Selection.Font
+        .ThemeColor = xlThemeColorAccent3
+        .TintAndShade = 0.599993896298105
+    End With
+End Sub
