a8b7c081af137f73add8cc0624fc9223d423af62
HernaldoMeneses <hernaldomeneses@gmail.com>
2023-11-02 06:49:12 -0300

    /C

---
+++ ABC-I/C/01_init_04.c
@@ -0,0 +1,69 @@
+//########### - Guia Inicial.
+//#
+//# - Objetivo: Primeiro contato com a linguagem R e O RStudio
+//# - Obs     : Sem Exaurimento.
+//# -     * Topicos Abordados
+//# -         Variáveis
+//# -             Criação
+//# -             Atribuição
+//# -             R Case Sensitive
+//# -             Strings
+//#
+//# - Infrastrutura de compilação e informações ao final do Script.
+//########### --------------------------------------------------------------------Thanks.
+
+//########### - Parte I - inclusao das bibliotecas.
+#include <stdio.h>
+
+//#main function
+/*
+Longs Coments
+*/
+int main() {
+    /*
+    *
+    *
+    * 
+    * */
+    printf("Hello.\n");
+    
+    //declare x var
+    int x;
+    int another_var;
+ 
+    another_var = 30;
+    x = another_var;
+    printf("value var  %i .\n", x);
+
+    float sf,si, sv;
+    float tf,ti, tv;
+    float vm;
+    si=-5;
+    sf=10;
+    tf=8;
+    ti=3;
+    sv = sf-si;
+    tv = tf-ti;
+    vm = sv/tv;
+    printf("A variavel tem valor  %f .\n", sv);
+    printf("endSpace  %f m - initSpace %f m varSpace %f m .\n", sf,si,sv);
+    printf("endTime  %f s - initTime %f s varTime %f s .\n", tf,ti,tv);
+    printf("speedMed  %f m/s .\n", vm);
+
+
+    return 0;
+}
+
+
+//########### --------------------------------------------------------------------Reloaded.
+
+
+//########### - Compile_By.
+//#
+//# - SO:  Windows 10 Pro.22H2
+//# - IDE: RStudio.2023.09.0
+//# - Obs: no bugs.
+//# - Obs: 01/10/2023.
+//#
+//# - Hello... please if you find something wrong,  contact-me.
+//########### --------------------------------------------------------------------Follow the white rabbit.
