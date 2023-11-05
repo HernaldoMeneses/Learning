a2de2872238c082fd8f3b861c02f8d2f93a3304b
HernaldoMeneses <hernaldomeneses@gmail.com>
2023-11-05 00:42:46 -0300

    °°

--- ABC-I/R/Data-Analitycs/init.r
+++ ABC-I/R/Data-Analitycs/init.r
@@ -1,2 +1,3 @@
 # PlayGround
-# https://www.dadosmundiais.com/downloads/
+# $$$$ https://www.dadosmundiais.com/downloads/
+# https://fivethirtyeight.com/features/dont-blame-a-skills-gap-for-lack-of-hiring-in-manufacturing/
