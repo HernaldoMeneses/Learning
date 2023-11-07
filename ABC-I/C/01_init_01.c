79356ba715d98889da9c44f162a39cc96c9d0a85
HernaldoMeneses <hernaldomeneses@gmail.com>
2023-11-07 05:29:38 -0300

    swith c

--- ABC-I/C/01_init_01.c
+++ ABC-I/C/01_init_01.c
@@ -1,37 +1,29 @@
+//###################### -|       Product by - LineByLine        |
+//###################### -|       Direct by - Hernaldo Meneses   |
 //########### - Guia Inicial.
 //#
-//# - Objetivo: Primeiro contato com a linguagem R e O RStudio
+//# - Objetivo: Primeiro contato com a C e Compilador gcc
 //# - Obs     : Sem Exaurimento.
 //# -     * Topicos Abordados
-//# -         Variáveis
-//# -             Criação
-//# -             Atribuição
-//# -             R Case Sensitive
-//# -             Strings
-//#
+//# -         # inlcude <stdio.h>
+//# -         # int main() {return 0;}
+//# -         # printf("Hello.\n");
 //# - Infrastrutura de compilação e informações ao final do Script.
 //########### --------------------------------------------------------------------Thanks.
-
 //########### - Parte I - inclusao das bibliotecas.
 #include <stdio.h>
-
-//#main function
-int main() {
-	printf("Hello.\n");
-    printf("Hello.");
-    printf("Hello.\n");
-
-    return 0;
-}
-
-
+    //#main function
+    int main() {
+        printf("Hello.\n");
+        printf("Hello.");
+        printf("Hello.\n");
+    return 0;}
 //########### --------------------------------------------------------------------Reloaded.
-
-
 //########### - Compile_By.
 //#
 //# - SO:  Windows 10 Pro.22H2
-//# - IDE: RStudio.2023.09.0
+//# - IDE: prompt cmd c:\Users\%user%\gcc 01_init_01.c
+//# - Run: prompt cmd c:\Users\%user%\a.exe
 //# - Obs: no bugs.
 //# - Obs: 01/10/2023.
 //#
---
+++ ABC-I/C/01_init_09.c
@@ -0,0 +1,47 @@
+//########### - Guia Inicial.
+//#
+//# - Objetivo: Primeiro contato com a linguagem C
+//# - Obs     : Sem Exaurimento.
+//# -     * Topicos Abordados
+//# -         switch
+//#
+//# - Infrastrutura de compilação e informações ao final do Script.
+//########### --------------------------------------------------------------------Thanks.
+#include <stdio.h>
+int main() {
+    int i;
+    printf("Insira um numero inteiro de 1 a 5\n");
+    scanf("%i", &i);
+    switch (i) {
+        case 1:
+            printf("primeiro");
+            break;
+        case 2:
+            printf("segundo");
+            break;
+        case 3:
+            printf("terceiro");
+            break;
+        case 4:
+            printf("quarto");
+            break;
+        case 5:
+            printf("quinto");
+            break;
+        default:
+            printf("opcao invalida");
+            break;
+    }
+return 0;}
+//########### --------------------------------------------------------------------Reloaded.
+
+
+//########### - Compile_By.
+//#
+//# - SO:  Windows 10 Pro.22H2
+//# - IDE: gcc
+//# - Obs: just ggc script.name on prompt
+//# - Obs: 06/11/2023
+//#
+//# - Hello... please if you find something wrong,  contact-me.
+//########### --------------------------------------------------------------------Follow the white rabbit.
