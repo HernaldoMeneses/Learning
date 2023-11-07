678cca893748f18fc6cd24ba320d00ae3f0ac100
HernaldoMeneses <hernaldomeneses@gmail.com>
2023-11-07 20:40:21 -0300

    Update 01_init_01.cmd

--- ABC-I/Bach/01_init_01.cmd
+++ ABC-I/Bach/01_init_01.cmd
@@ -1,30 +1,17 @@
-##Hello
-# O Console cmd.exe:
-# Comandos Basicos
-#Clear
 Cls 
-## apresentar 
 Echo
 Echo on
 Echo off
-## Copiar saida do comando anterior para area de tranferencia
 Echo Hernaldo Meneses | clip
-
-## Informações dos discos
 Vol
-
 Ver
 Clip
 Date
 Time
-## iniciar outras tarefas
-## util para iniciar programans que não estão no 
-## previamente inseridas no 'path'
 Start notepad
 Start www.googl.com
 Start mrt
 Start "New windows prompt" cmd.exe
-
 Winver
 echo Hernaldo Meneses | cmd.exd
 pause
