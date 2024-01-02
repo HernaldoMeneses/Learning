b4a1d62dca1cc80f1ead6e6d818194387ecdface
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-01-02 14:34:31 -0300

    Update 01-HelloWorld.js

--- ABC-I/JavaScript/01-HelloWorld.js
+++ ABC-I/JavaScript/01-HelloWorld.js
@@ -1,1 +1,28 @@
-alert('Hello Neo, The Matrix has you...')
+var sms = 'Hello Neo, The Matrix has you...'; // {Strings}
+var sms2 = 'Primitive Types for JavaScript.';
+var sms3 = 1; // {int}
+var sms4 = 1.5; // {float}
+var sms6 = true; //{true, false} - bolean
+var sms7 = null; // {null}
+var sms8; // {Undefined}
+
+function fx__lineBy(var_, _type_){
+    document.write('<br> Hello _ Functions <br>', ' ',var_, ' ', _type_, '<br>');
+    return '';
+}
+
+
+alert(sms);
+alert(sms2);
+
+
+document.write('int type: ' + sms3, '<br>');
+document.write('float type: ', sms4 , '<br>');
+document.write('bolean type: ', sms6 , '<br>');
+document.write('null type: ', sms7 , '<br>');
+document.write('undefined type: ', sms8 , '<br>');
+
+var typee = 'int';
+document.write(fx__lineBy(sms3, typee));
+
+//Cont... 40/449
